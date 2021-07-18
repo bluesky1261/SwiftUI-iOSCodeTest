e4f3869752ab9cdc9829b145d8bb0ccc0fed3537
Joonghoo Im <bluesky1261@gmail.com>
2021-07-18 22:47:34 +0900

    최초 커밋

---
+++ iOSCodeTest/iOSCodeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSCodeTestApp.swift
+//  iOSCodeTest
+//
+//  Created by Joonghoo Im on 2021/07/18.
+//
+
+import SwiftUI
+
+@main
+struct iOSCodeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
